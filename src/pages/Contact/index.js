f726ca9b135a03434e7c86b70a75067b3315a02a
卓利云 <lynn@ub-am.com>
2019-03-06 16:46:18 +0800

    fix(contact):add animotion

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,10 +3,13 @@ import './index.css';
 import papa from '../../assets/avatar_papa.jpeg';
 import husband from '../../assets/avatar_husband.jpeg';
 import mama from '../../assets/avatar_mama.jpg';
-import cousin from '../../assets/avatar_cousin.jpeg';
+// import cousin from '../../assets/avatar_cousin.jpeg';
+import all from '../../assets/icon_all.jpg';
 
 class Contact extends React.Component {
-    state = {
+    constructor(props) {
+      super(props);
+      this.state = {
         isSubMenu: false,
         tempPeopleList: [{
             id: 1,
@@ -30,10 +33,12 @@ class Contact extends React.Component {
             id: 4,
             name: 'Elisabeth Carson',
             appellation: 'Cousin',
-            avatar: cousin,
+            // avatar: cousin,
             isSelect: false,
         }],
+      };
     }
+
 
     subMenuHidden = () => {
       const {isSubMenu} = this.state;
@@ -58,13 +63,27 @@ class Contact extends React.Component {
           <div className="contact-wrap" >
             <div className="contact-top-category" onClick={this.subMenuHidden}>
               <div className="contact-title">PEOPLE</div>
+              <div className={`right-arrow ${isSubMenu? 'vertical': ''}`} />
             </div>
            <div className={`contact-list ${isSubMenu? '': 'subMenuHidden'}`}>
+              <div className="contact-list-Item">
+                  <div className="grey" />
+                  <div className="people-cell" >
+                    <div className="people-avatar" style={{ backgroundImage: `url(${all})` }}/>
+                    <div>
+                        <div className="people-name">ALL</div>
+                    </div>
+                  </div>
+                </div>
               {tempPeopleList.map((v, index)=>(
                 <div className="contact-list-Item">
                   <div className={v.isSelect ? 'green' : 'grey'} />
                   <div className={`people-cell${v.isSelect ? ' isSelect' : ''}`} key={v.id} onClick={this.selectPeople(index)}>
-                    <div className="people-avatar" style={{ backgroundImage: `url(${v.avatar})` }}/>
+                    {v.avatar?
+                      <div className="people-avatar" style={{ backgroundImage: `url(${v.avatar})` }}/>
+                      :
+                      <div className="people-avatar-none" />
+                    }
                     <div>
                         <div className="people-name">{v.name}</div>
                         <div className="people-appellation">{v.appellation}</div>
